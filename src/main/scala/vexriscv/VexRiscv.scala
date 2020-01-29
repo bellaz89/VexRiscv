012a17ec0b1f6beac88c001516c60a82e51cbcce
Andrea Bellandi <andrea.bellandi@desy.de>
2020-01-29 09:14:32 +0100

    readded float in the main Riscv/VexRiscv modules

--- src/main/scala/vexriscv/VexRiscv.scala
+++ src/main/scala/vexriscv/VexRiscv.scala
@@ -2,24 +2,103 @@ package vexriscv
 
 import vexriscv.plugin._
 import spinal.core._
+import spinal.lib.float._
 
 import scala.collection.mutable.ArrayBuffer
 
-object VexRiscvConfig{
-  def apply(withMemoryStage : Boolean, withWriteBackStage : Boolean, plugins : Seq[Plugin[VexRiscv]]): VexRiscvConfig = {
-    val config = VexRiscvConfig()
-    config.plugins ++= plugins
-    config.withMemoryStage = withMemoryStage
-    config.withWriteBackStage = withWriteBackStage
-    config
+object RiscvRoundingMode extends SpinalEnum {
+  val RNE = newElement() // Round to Nearest, ties to Even
+  val RTZ = newElement() // Round towards Zero
+  val RDN = newElement() // Round Down (towards -inf)
+  val RUP = newElement() // Round Up   (towards +inf) 
+  val RMM = newElement() // Round to Nearest, ties to Max Magnitude
+  val DYN = newElement() // In instruction's rm field, selects dynamic
+  val INV = newElement()
+
+  def roundingFromBits(b : Bits) : RiscvRoundingMode.C = {
+    val r = RiscvRoundingMode()
+
+    switch(b){
+      is(B"000") { r := RNE }
+      is(B"001") { r := RTZ }
+      is(B"010") { r := RDN }
+      is(B"011") { r := RUP }
+      is(B"100") { r := RMM }
+      is(B"111") { r := DYN }
+      default { r := INV }
+    }
+    r
   }
 
-  def apply(plugins : Seq[Plugin[VexRiscv]] = ArrayBuffer()) : VexRiscvConfig = apply(true,true,plugins)
+  def bitsFromRounding(r : RiscvRoundingMode.C) : Bits = {
+    val b = Bits(3 bits)
+    switch(r) {
+      is(RNE) { b := B"000" }
+      is(RTZ) { b := B"001" }
+      is(RDN) { b := B"010" }
+      is(RUP) { b := B"011" }
+      is(RMM) { b := B"100" }
+      is(DYN) { b := B"111" }
+      default { b := B"101" }
+    }
+    b
+  }
+}
+
+class RiscvFloatExceptions extends Bundle {
+
+  val NV = Bool
+  val DZ = Bool
+  val OF = Bool
+  val UF = Bool
+  val NX = Bool
+
+  def orExceptions(e : RiscvFloatExceptions) : RiscvFloatExceptions = {
+    val or = new RiscvFloatExceptions
+    or.NV := e.NV | NV
+    or.DZ := e.DZ | DZ
+    or.OF := e.OF | OF
+    or.UF := e.UF | UF
+    or.NX := e.NX | NX
+    or
+  }
+}
+
+
+object VexRiscvConfig{
+  def apply(withMemoryStage : Boolean, 
+            withWriteBackStage : Boolean, 
+            plugins : Seq[Plugin[VexRiscv]]): VexRiscvConfig = {
+
+      val config = VexRiscvConfig()
+      config.plugins ++= plugins
+      config.withMemoryStage = withMemoryStage
+      config.withWriteBackStage = withWriteBackStage
+      config.withFloat = false
+      config
+  }
+
+  def apply(withMemoryStage : Boolean, 
+            withWriteBackStage : Boolean, 
+            plugins : Seq[Plugin[VexRiscv]],
+            withFloat : Boolean): VexRiscvConfig = {
+
+      val config = VexRiscvConfig()
+      config.plugins ++= plugins
+      config.withMemoryStage = withMemoryStage
+      config.withWriteBackStage = withWriteBackStage
+      config.withFloat = withFloat
+      config
+  }
+  
+  def apply(plugins : Seq[Plugin[VexRiscv]] = ArrayBuffer()) : VexRiscvConfig = apply(true,true,plugins,false)
+  def withFloat(plugins : Seq[Plugin[VexRiscv]] = ArrayBuffer()) : VexRiscvConfig = apply(true,true,plugins,true)
 }
 
 case class VexRiscvConfig(){
   var withMemoryStage = true
   var withWriteBackStage = true
+  var withFloat = false
   val plugins = ArrayBuffer[Plugin[VexRiscv]]()
 
   def add(that : Plugin[VexRiscv]) : this.type = {plugins += that;this}
@@ -79,6 +158,38 @@ case class VexRiscvConfig(){
 
   object SRC1_CTRL  extends Stageable(Src1CtrlEnum())
   object SRC2_CTRL  extends Stageable(Src2CtrlEnum())
+
+  // F extension
+
+  object FLOAT_RS1 extends Stageable(new RecFloat33)
+  object FLOAT_RS2 extends Stageable(new RecFloat33)
+  object FLOAT_RS3 extends Stageable(new RecFloat33)
+  object FLOAT_RS1_USE extends Stageable(Bool)
+  object FLOAT_RS2_USE extends Stageable(Bool)
+  object FLOAT_RS3_USE extends Stageable(Bool)
+  object FLOAT_RESULT extends Stageable(new RecFloat33)
+  object FLOAT_REGFILE_WRITE_VALID extends Stageable(Bool)
+  object FLOAT_REGFILE_WRITE_DATA extends Stageable(new RecFloat33)
+  object FLOAT_EXCEPTION extends Stageable(new RiscvFloatExceptions)
+  object FLOAT_ROUNDING extends  Stageable(RiscvRoundingMode())
+  object FLOAT_USE_ROUNDING extends Stageable(Bool)
+
+  object FloatSrc1CtrlEnum extends SpinalEnum(binarySequential){
+    val RS = newElement()
+  }
+
+  object FloatSrc2CtrlEnum extends SpinalEnum(binarySequential){
+    val RS, IMI, IMS = newElement()
+  }
+
+  object FloatSrc3CtrlEnum extends SpinalEnum(binarySequential){
+    val RS  = newElement()
+  }
+  object FLOAT_SRC1_CTRL  extends Stageable(FloatSrc1CtrlEnum())
+  object FLOAT_SRC2_CTRL  extends Stageable(FloatSrc2CtrlEnum())
+  object FLOAT_SRC3_CTRL  extends Stageable(FloatSrc3CtrlEnum())
+
+
 }
 
 
